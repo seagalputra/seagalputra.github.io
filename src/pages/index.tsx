c4b582c841d5e8eb71951076ba9da9bf4f5ef4f0
seagalputra <seagalputra@gmail.com>
2021-01-10 16:17:16 +0700

    Adding under construction information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: React.FC<PageProps<{}>> = () => {
   return (
     <Layout>
       <SEO title="Home" description="My Personal Blog" lang="en" />
-      <div className="prose max-w-none">Hello, World</div>
+      <div className="prose max-w-none">This site is under construction!</div>
     </Layout>
   )
 }
